87f206a1267fa8136c749cf6692e715b44350d97
pyar6329 <pyar6329@gmail.com>
2016-01-15 22:02:36 +0900

    add expires rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,29 @@ class User < ActiveRecord::Base
   def self.find_for_google_oauth2(auth)
     user = User.where(email: auth.info.email).first
 
+    # tokenが失効時、ユーザー情報を更新
+    unless auth.credentials.expires
+      user.update(name: auth.info.name,
+                  provider: auth.provider,
+                  uid: auth.uid,
+                  email: auth.info.email,
+                  token: auth.credentials.token,
+                  password: Devise.friendly_token[0, 20],
+                  expires_at: auth.credentials.expires_at,
+                  expires: auth.credentials.expires
+                 )
+    end
+    # ユーザーが居ない場合、作成する
     unless user
       user = User.create(name: auth.info.name,
                          provider: auth.provider,
                          uid: auth.uid,
                          email: auth.info.email,
                          token: auth.credentials.token,
-                         password: Devise.friendly_token[0, 20])
+                         password: Devise.friendly_token[0, 20],
+                         expires_at: auth.credentials.expires_at,
+                         expires: auth.credentials.expires
+                         )
     end
     user
   end
