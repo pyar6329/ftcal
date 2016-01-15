34d2632635548d2e2de2d227eae8cc86ea59656d
pyar6329 <pyar6329@gmail.com>
2016-01-15 22:03:46 +0900

    until implements

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,9 +1,31 @@
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+
 class CalendarController < ApplicationController
   before_action :authenticate_user!
-  include GoogleCalendarAPI
+  # include GoogleCalendarAPI
 
   def index
-    @google_oauth2_info = hoge # concernのメソッドを実行
+
+    client = Google::APIClient.new(application_name: 'xxx', application_version: '0.0.1')
+    client.authorization.client_id = ENV['GOOGLE_CLIENT_ID']
+    client.authorization.client_secret = ENV['GOOGLE_CLIENT_SECRET']
+    client.authorization.access_token = current_user.token
+    client.authorization.scope = 'https://www.googleapis.com/auth/calendar.readonly'
+
+    calendar = client.discovered_api('calendar', 'v3')
+
+    params = {'calendarId' => current_user.email,
+              'orderBy' => 'startTime',
+              'timeMax' => Time.utc(2014, 3, 31, 0).iso8601,
+              'timeMin' => Time.utc(2014, 3, 1, 0).iso8601,
+              'singleEvents' => 'True'}
+
+    event = client.execute(api_method: calendar.events.list,
+                            parameters: params)
+    @set_event = event
+    # @set_event = current_user.to_json
+    # @google_oauth2_info = hoge # concernのメソッドを実行
     # @google_oauth2_info = current_user.token
   end
 end
--- app/controllers/concerns/google_calendar_api.rb
+++ app/controllers/concerns/google_calendar_api.rb
@@ -10,36 +10,36 @@ module GoogleCalendarAPI
     # https://github.com/google/google-api-ruby-client-samples/blob/master/calendar/calendar.rb
     # https://github.com/google/google-api-ruby-client/blob/master/lib/google/api_client/client_secrets.rb
     # puts "hellllllllllllllllllllllllllllllllllllllo"
-    client = Google::APIClient.new
-    client.authorization.access_token = current_user.token
-    calendar = client.discovered_api('calendar', 'v3')
-    year = 2016
-    month = 1
-    calendar_params = { 'calendarId' => current_user.email,
+    # client = Google::APIClient.new
+    # client.authorization.access_token = current_user.token
+    # calendar = client.discovered_api('calendar', 'v3')
+    # year = 2016
+    # month = 1
+    # calendar_params = { 'calendarId' => current_user.email,
                         # 'orderBy' => 'startTime',
                         # 'timeMax' => Time.utc(year, month, 31, 0).iso8601,
                         # 'timeMin' => Time.utc(year, month, 1, 0).iso8601,
                         # 'singleEvents' => 'True',
-                        'sendNotifications' => true
-                      }
-    event = {
-      'summary' => 'New Event Title',
-      'description' => 'The description',
-      'location' => 'Location',
-      'start' => { 'dateTime' => Chronic.parse('tomorrow 4 pm') },
-      'end' => { 'dateTime' => Chronic.parse('tomorrow 5pm') },
-      'attendees' => [{ "email" => 'hoge@gmail.com' }]
-    }
-
-    result = client.execute(api_method: calendar.events.list,
-                            parameters: calendar_params,
-                            body: JSON.dump(event),
-                            headers: {'Content-Type' => 'application/json'})
+    #                     'sendNotifications' => true
+    #                   }
+    # event = {
+    #   'summary' => 'New Event Title',
+    #   'description' => 'The description',
+    #   'location' => 'Location',
+    #   'start' => { 'dateTime' => Chronic.parse('tomorrow 4 pm') },
+    #   'end' => { 'dateTime' => Chronic.parse('tomorrow 5pm') },
+    #   'attendees' => [{ "email" => 'hoge@gmail.com' }]
+    # }
+    #
+    # result = client.execute(api_method: calendar.events.list,
+    #                         parameters: calendar_params,
+    #                         body: JSON.dump(event),
+    #                         headers: {'Content-Type' => 'application/json'})
     # events = []
     # result.data.items.each do |item|
       # events.push(item)
     # end
     # return events
-    return result
+    # return result
   end
 end
