681b2c602b1f9cdcb5a49689f4b9896020b96350
pyar6329 <pyar6329@gmail.com>
2016-04-30 22:12:21 +0900

    string to hash

--- spec/support/oauth_macros.rb
+++ spec/support/oauth_macros.rb
@@ -1,84 +1,53 @@
 module OauthMacros
   # omniauthで取得するユーザーデータの作成
-  # def get_omniauth_hash
-  #   user_data = OmniAuth::AuthHash.new({
-  #     "provider" => "google_oauth2",
-  #     "uid" => generate(:uid),
-  #     "info" => {
-  #       "name" => generate(:name),
-  #       "email" => generate(:email)
-  #     },
-  #     "credentials" => {
-  #       "token" => generate(:password),
-  #       "refresh_token" => generate(:password),
-  #       "expires_at" => generate(:expires_at),
-  #       "expires" => "true"
-  #     }
-  #   })
-  #   user_data
-  # end
-
   def get_omniauth_hash
     user_data = OmniAuth::AuthHash.new({
-      :provider => "google_oauth2",
-      :uid => "123456789",
-      :info => {
-          :name => "John Doe",
-          :email => "john@company_name.com",
-          :first_name => "John",
-          :last_name => "Doe",
-          :image => "https://lh3.googleusercontent.com/url/photo.jpg"
+      provider: "google_oauth2",
+      uid: generate(:uid),
+      info: {
+        name: generate(:name),
+        email: generate(:email),
+        first_name: generate(:first_name),
+        last_name: generate(:last_name),
+        image: generate(:avatar_image)
       },
-      :credentials => {
-          :token => "token",
-          :refresh_token => "another_token",
-          :expires_at => 1354920555,
-          :expires => true
+      credentials: {
+        token: generate(:password),
+        refresh_token: generate(:password),
+        expires_at: generate(:expires_at),
+        expires: true
       },
-      :extra => {
-          :raw_info => {
-              :sub => "123456789",
-              :email => "user@domain.example.com",
-              :email_verified => true,
-              :name => "John Doe",
-              :given_name => "John",
-              :family_name => "Doe",
-              :profile => "https://plus.google.com/123456789",
-              :picture => "https://lh3.googleusercontent.com/url/photo.jpg",
-              :gender => "male",
-              :birthday => "0000-06-25",
-              :locale => "en",
-              :hd => "company_name.com"
-          },
-          :id_info => {
-              "iss" => "accounts.google.com",
-              "at_hash" => "HK6E_P6Dh8Y93mRNtsDB1Q",
-              "email_verified" => "true",
-              "sub" => "10769150350006150715113082367",
-              "azp" => "APP_ID",
-              "email" => "jsmith@example.com",
-              "aud" => "APP_ID",
-              "iat" => 1353601026,
-              "exp" => 1353604926,
-              "openid_id" => "https://www.google.com/accounts/o8/id?id=ABCdfdswawerSDFDsfdsfdfjdsf"
-          }
+      extra: {
+        raw_info: {
+          sub: "123456789",
+          email: generate(:email),
+          email_verified: true,
+          name: generate(:name),
+          given_name: generate(:first_name),
+          family_name: generate(:last_name),
+          profile: generate(:url),
+          picture: generate(:avatar_image),
+          gender: generate(:gender),
+          birthday: "0000-06-25",
+          locale: "en",
+          hd: generate(:last_name)
+        },
+        id_info: {
+          "iss" => "accounts.google.com",
+          "at_hash" => generate(:password),
+          "email_verified" => "true",
+          "sub" => "10769150350006150715113082367",
+          "azp" => "APP_ID",
+          "email" => generate(:email),
+          "aud" => "APP_ID",
+          "iat" => 1353601026,
+          "exp" => 1353604926,
+          "openid_id" => generate(:url)
+        }
       }
-      # provider: "google_oauth2",
-      # uid: generate(:uid),
-      # info: {
-      #   name: generate(:name),
-      #   email: generate(:email)
-      # },
-      # credentials: {
-      #   token: generate(:password),
-      #   refresh_token: generate(:password),
-      #   expires_at: generate(:expires_at),
-      #   expires: true
-      # }
     })
     user_data
   end
-
 
   # 有効なログイン(controller_spec)
   def set_valid_omniauth_callback
@@ -103,9 +72,5 @@ module OauthMacros
   # ログイン
   def login_user_as(user)
     login_as user, scope: :user
-    # @request.env["devise.mapping"] = Devise.mappings[:user]
-    # # user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-    # sign_in user
-    # user
   end
 end
